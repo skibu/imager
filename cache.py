588333f17a9567a7c74216e07464382e1ac80017
Michael Smith <mike@goswift.ly>
2024-07-30 11:16:47 -0700

    Made file name hash just 12 chars long. 16 chars is way more than needed, and shorter file names can be nice.

--- cache.py
+++ cache.py
@@ -7,11 +7,11 @@ def stable_hash_str(key: str) -> str:
     """
     A hash function that is consistent across restarts. That is of course important for file names for a cache.
     :param key: string to be hashed.
-    :return: a 16 character hash string
+    :return: a 12 character hex hash string
     """
     str_bytes = bytes(key, "UTF-8")
     m = hashlib.md5(str_bytes)
-    return m.hexdigest()[:16].upper()
+    return m.hexdigest()[:12].upper()
 
 
 def proper_filename(filename):
