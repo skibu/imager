126271db4fec01f292e9ca49ad104fcaf7f1f1cc
Michael Smith <mike@goswift.ly>
2024-08-30 15:41:08 -0700

    Better handle mostly silent clips. Changed silence_thresh from -70 to -74. Also made sure that get at least minimum bird song length of 7 seconds when detects possible second voice section.

--- audio.py
+++ audio.py
@@ -73,12 +73,13 @@ def get_wav_file(handler: BaseHTTPRequestHandler):
     sound = AudioSegment.from_mp3(BytesIO(mp3.content))[:max_clip_voice_msec + max_clip_msec]
 
     # Try to get rid of any voice introduction to the clip. The voice intros appear to be consistently
-    # separated by half second or so of silence. Found that had to reduce the silence_thresh to -70.0 even
-    # though the db of a non-silent clip was just -38. But at least it works. And found that it is really
+    # separated by half second or so of silence. Found that had to reduce the silence_thresh to -74.0 even
+    # though the db of a non-silent clip was just -38. But at least it works. Had tried -70.0 but was getting
+    # too many false silent sections. And found that it is really
     # important to set seek_step to 20 since that makes the function run 20x, though then the boundaries are
     # of course not determined as accurately. And found that needed small min_silence_len=50 so that would
     # work for ML106633 wav file.
-    silent_portions = silence.detect_silence(sound, min_silence_len=50, silence_thresh=-70.0, seek_step=20)
+    silent_portions = silence.detect_silence(sound, min_silence_len=50, silence_thresh=-74.0, seek_step=20)
 
     initial_duration_msec = sound.duration_seconds * 1000
     end_of_last_voice_silence = None
@@ -97,10 +98,17 @@ def get_wav_file(handler: BaseHTTPRequestHandler):
 
         # Look for first silence past max_clip_voice_msec, which probably indicates a second voice.
         # Example is  https://cdn.download.ams.birds.cornell.edu/api/v2/asset/29385/mp3&s=Short-billed_Dowitcher
+        # But also make sure that get a minimum of bird song. Otherwise probably just have intermittent silent
+        # portions instead of a second voice portion.
+        # Example: url=https://cdn.download.ams.birds.cornell.edu/api/v2/asset/125383/mp3 species=Hermit Warbler
         if start > max_clip_voice_msec:
-            logger.info(f'Additional voice section found so trimming it off from the audio')
-            special_end_found = start
-            break
+            if start > end_of_last_voice_silence + min_bird_sound_msec:
+                special_end_found = start
+                logger.info(f'Additional possible voice section found so trimming it off from the audio at '
+                            f'special_end_found={special_end_found} silence stop={stop}')
+                break
+            else:
+                logger.info(f'Found another silence but was in bird song part. start={start} stop={stop}')
 
     # Trim the sound to get rid of voice parts
     non_voice_start = 0 if end_of_last_voice_silence is None else end_of_last_voice_silence
@@ -109,6 +117,7 @@ def get_wav_file(handler: BaseHTTPRequestHandler):
 
     # Trim sound clip to final length now that have removed any possible intro
     sound = sound[0:max_clip_msec]
+    logger.info(f'Resulting audio clip is {sound.duration_seconds} seconds long')
 
     # Normalize sound so loud as possible
     sound = normalize(sound, headroom=1.0)
